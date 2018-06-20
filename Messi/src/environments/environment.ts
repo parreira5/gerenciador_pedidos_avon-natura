95a38aa7afddaef0c1a7d6b2749060110d85f4bb
Felipe Messi <felipe.192@hotmail.com>
2018-06-20 03:29:34 -0300

    integração com Firebase

--- Messi/src/environments/environment.ts
+++ Messi/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAFM5A0oRybunlkv_88ae2IymRic-HS6_M",
+    authDomain: "projetotcc-7a495.firebaseapp.com",
+    databaseURL: "https://projetotcc-7a495.firebaseio.com",
+    projectId: "projetotcc-7a495",
+    storageBucket: "projetotcc-7a495.appspot.com",
+    messagingSenderId: "620616593183"
+  }
 };
 
 /*
@@ -12,4 +20,4 @@ export const environment = {
  * import the following file, but please comment it out in production mode
  * because it will have performance impact when throw error
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/dist/zone-error';  // Included with Angular CLI.
